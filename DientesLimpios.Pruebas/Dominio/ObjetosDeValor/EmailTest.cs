ca49d2221418f8f47e3a4d68ab959e0eb439e689
Edwin Marshall <edwin.lozanoe@outlook.com>
2026-04-03 16:59:27 -0600

    Agrega pruebas unitarias para objetos de valor Email e IntervaloDeTiempo

---
+++ DientesLimpios.Pruebas/Dominio/ObjetosDeValor/EmailTest.cs
@@ -0,0 +1,38 @@
+﻿using DientesLimpios.Dominio.Excepciones;
+using DientesLimpios.Dominio.ObjetosDeValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Pruebas.Dominio.ObjetosDeValor
+{
+    public class EmailTest
+    {
+        [Fact]
+        public void Constructor_EmailNulo_LanzaExcepcion()
+        {
+            // Vamos a probar que el constructor de la clase Email lance una ExcepcionDeReglaDeNegocio cuando se le pase un valor nulo.
+            Assert.Throws<ExcepcionDeReglaDeNegocio>(
+                () => new Email(null!)
+            );
+        }
+
+        [Fact]
+        public void Constructor_EmailSinArroba_LanzaExcepcion()
+        {
+            Assert.Throws<ExcepcionDeReglaDeNegocio>(() => new Email("edwin.com"));
+        }
+
+        [Fact]
+        public void Constructor_EmailValido_CreaInstancia()
+        {
+            var email = new Email("edwin@gmail.com");
+
+            Assert.NotNull(email);
+            Assert.Equal("edwin@gmail.com", email.Valor);
+        }
+
+    }
+}
---
+++ DientesLimpios.Pruebas/Dominio/ObjetosDeValor/IntervaloDeTiempoTest.cs
@@ -0,0 +1,29 @@
+﻿using DientesLimpios.Dominio.Excepciones;
+using DientesLimpios.Dominio.ObjetosDeValor;
+
+namespace DientesLimpios.Pruebas.Dominio.ObjetosDeValor
+{
+    public class IntervaloDeTiempoTest
+    {
+        [Fact]
+        public void Constructor_FechaInicioMayorAFechaFin_LanzaExcepcion()
+        {
+            DateTime fechaInicio = DateTime.UtcNow.AddDays(10);
+            DateTime fechaFin = DateTime.UtcNow;
+
+            Assert.Throws<ExcepcionDeReglaDeNegocio>(() => new IntervaloDeTiempo(fechaInicio, fechaFin));
+        }
+
+        [Fact]
+        public void Constructor_FechaInicioMenorAFechaFin_CreaIntervaloDeTiempo()
+        {
+            DateTime fechaInicio = DateTime.UtcNow;
+            DateTime fechaFin = DateTime.UtcNow.AddMinutes(30);
+
+            var intervalo = new IntervaloDeTiempo(fechaInicio, fechaFin);
+
+            Assert.NotNull(intervalo);
+            Assert.True(intervalo.Inicio < intervalo.Fin);
+        }
+    }
+}
