c296852f2fbb86cf0ce746731dbe587b6c295d85
Edwin Marshall <edwin.lozanoe@outlook.com>
2026-04-07 22:59:01 -0600

    COnfigurando las dependencias del proyecto y la base de datos con migracion

--- DientesLimpios.API/Program.cs
+++ DientesLimpios.API/Program.cs
@@ -1,8 +1,14 @@
+using DientesLimpios.Aplicacion;
+using DientesLimpios.Persistencia;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AgregarServiciosDeAplicacion();
+builder.Services.AgregarServiciosDePersistencia();
 
 var app = builder.Build();
 
